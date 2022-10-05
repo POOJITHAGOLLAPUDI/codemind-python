d1d9de15c19426c96fff9f978c69c4710c483f6f
poojithagollapudi <84671939+POOJITHAGOLLAPUDI@users.noreply.github.com>
2022-10-05 09:44:59 +0530

    for i in range(int(input())):     l,r=map(int,input().split())     s=0     for i in range(l,r+1):         if i%10==2 or i%10==3 or i%10==9:             s+=1     print(s)

---
+++ _Counting_Pretty_Numbers.py
@@ -0,0 +1,8 @@
+for i in range(int(input())):
+    l,r=map(int,input().split())
+    s=0
+    for i in range(l,r+1):
+        if i%10==2 or i%10==3 or i%10==9:
+            s+=1
+    print(s)
+            
